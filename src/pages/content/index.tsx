ad23fbe724d1f47c22822cfdbcbf3abce62c6aa4
Chuck <403chuck@gmail.com>
2024-04-14 23:25:12 +0800

    refactor: remove useless component

--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { createRoot } from "react-dom/client";
 
 function Content(): JSX.Element {
+
+  useEffect(() => {
+    console.log("Content mounted");
+    const handleMouseUp = (e: MouseEvent) => {
+      if (e.button != 0) {
+        return;
+      }
+      const selectedText = window.getSelection()?.toString();
+      console.log(selectedText);
+    }
+    document.addEventListener("mouseup", handleMouseUp);
+    return () => {
+      document.removeEventListener("mouseup", handleMouseUp);
+    }
+  }, []);
+
   return (
     <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800">
       <header className="flex flex-col items-center justify-center text-white">
@@ -23,7 +39,10 @@ function Content(): JSX.Element {
 }
 
 function init() {
-  const rootContainer = document.body;
+  const body = document.body;
+  const rootContainer = document.createElement("div");
+  rootContainer.id = "__root";
+  body.appendChild(rootContainer);
   if (!rootContainer) throw new Error("Can't find Content root element");
   const root = createRoot(rootContainer);
   root.render(<Content />);
--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,24 +1,17 @@
-import React from "react";
-
-import logo from "@assets/img/logo.svg";
+import React, { useEffect } from "react";
 
 export default function Popup(): JSX.Element {
+  useEffect(() => {
+    console.log("Popup mounted");
+  });
+
   return (
-    <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-lime-300">
-      <header className="flex flex-col items-center justify-center text-white">
-        <img src={logo} className="h-36 pointer-events-none animate-spin-slow" alt="logo" />
-        <p className="text-red-600">
-          Edit <code>src/pages/popup/Popup.jsx</code> and save to reload.
+    <div className="absolute top-0 left-0 right-0 bottom-0 h-full p-3 bg-white dark:bg-slate-900">
+      <header className="w-[100px] flex flex-col items-center justify-center text-white">
+        <p className="text-slate-800 dark:text-white">
+          test
         </p>
-        <a
-          className="text-blue-400"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-        <p className="aiyogg">Popup styled with TailwindCSS!</p>
+        
       </header>
     </div>
   );
